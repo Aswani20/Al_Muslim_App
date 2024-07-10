4598c2d04513387d8d85f1c7693d729a3d5c7000
aswani <abdoaswani1997@gmail.com>
2024-07-10 21:47:30 +0300

    Initial Project Empty project with home screen

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:al_muslim/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Al Muslim App",
+      initialRoute: HomeScreen.routeName,
+      routes: {
+        HomeScreen.routeName: (context) => HomeScreen(),
+      },
+    );
+  }
+}
